c67fa63cbdf18538f1dbe775b9984a8f6aa345d4
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-10-24 09:39:11 +0200

    my computer -> github

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function check_cookie(){
+
+};
+function accept_cookie(){
+    document.cookie = true
+};
+function deny_cookie (){
+
+};
+function set_cookie(){
+
+};
+function toggle_dark_mode() {
+    var element = document.getElementById("content");
+    var list    = element.classList;
+    list.toggle("dark_mode");
+ };
